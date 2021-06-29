c610fd2a3aea411aefd16a1501d57940913eff6e
worgflores <worgflores@gmail.com>
2021-06-29 12:02:19 +0800

    _res: patches: updated

--- _res/patches/02-patch-eor.sql
+++ _res/patches/02-patch-eor.sql
@@ -71,3 +71,66 @@ go
 
 alter table eor_share add receiptitemid varchar(50) null 
 go
+
+
+
+if object_id('dbo.vw_remittance_eor_item', 'V') IS NOT NULL 
+	drop view dbo.vw_remittance_eor_item; 
+go 
+CREATE VIEW vw_remittance_eor_item AS 
+select 
+	c.remittanceid AS remittanceid,
+	r.controldate AS remittance_controldate,
+	r.controlno AS remittance_controlno,
+	cri.parentid AS receiptid,
+	c.receiptdate AS receiptdate,
+	c.receiptno AS receiptno,
+	c.paidby AS paidby,
+	c.paidbyaddress AS paidbyaddress,
+	cri.item_fund_objid AS fundid,
+	cri.item_objid AS acctid,
+	cri.item_code AS acctcode,
+	cri.item_title AS acctname,
+	cri.remarks AS remarks,
+	cri.amount AS amount,
+	c.txntype 
+from eor_remittance r 
+	inner join eor c on c.remittanceid = r.objid 
+	inner join eor_item cri on cri.parentid = c.objid
+go 
+
+
+if object_id('dbo.vw_remittance_eor_share', 'V') IS NOT NULL 
+	drop view dbo.vw_remittance_eor_share; 
+go 
+CREATE VIEW vw_remittance_eor_share AS 
+select 
+	c.remittanceid AS remittanceid,
+	r.controldate AS remittance_controldate,
+	r.controlno AS remittance_controlno,
+	cri.parentid AS receiptid,
+	c.receiptdate AS receiptdate,
+	c.receiptno AS receiptno,
+	c.paidby AS paidby,
+	c.paidbyaddress AS paidbyaddress,
+	cri.refitem_objid AS refacctid,
+	cri.refitem_code AS refacctcode,
+	cri.refitem_title AS refaccttitle,
+	cri.payableitem_objid AS acctid,
+	cri.payableitem_code AS acctcode,
+	cri.payableitem_title AS acctname,
+	cri.share AS amount,
+	c.txntype  
+from eor_remittance r 
+	inner join eor c on c.remittanceid = r.objid 
+	inner join eor_share cri on cri.parentid = c.objid
+go 
+
+
+
+-- ## 2021-06-29
+INSERT INTO paymentpartner (objid, code, name, branch, contact, mobileno, phoneno, email, indexno) 
+VALUES ('PAYMAYA', '103', 'PAYMAYA', NULL, NULL, NULL, NULL, NULL, '103');
+
+INSERT INTO paymentpartner (objid, code, name, branch, contact, mobileno, phoneno, email, indexno) 
+VALUES ('GCASH', '104', 'GCASH', NULL, NULL, NULL, NULL, NULL, '104');
