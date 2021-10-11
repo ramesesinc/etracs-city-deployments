0d62f9a5858bf921117263c7f23de59007bc2199
Jessie S. Zamora <jzamss@gmail.com>
2021-10-11 16:04:17 +0800

    dev-modules: landtax: reports: real property payments and newly assessed properties

---
+++ _res/patches/etracs/2.5.04.04/mssql/02 - patch.sql
@@ -0,0 +1,50 @@
+create view vw_real_property_payment as 
+select 
+	cv.controldate as cv_controldate,
+	rem.controldate as rem_controldate,
+	rl.owner_name,
+	rl.tdno,
+	pc.name as classification, 
+	case 
+		when rl.rputype = 'land' then 'LAND' 
+		when rl.rputype = 'bldg' then 'BUILDING' 
+		when rl.rputype = 'mach' then 'MACHINERY' 
+		when rl.rputype = 'planttree' then 'PLANT/TREE' 
+		else 'MISCELLANEOUS'
+	end as rputype,
+	b.name as barangay,
+	rpi.amount + rpi.interest - rpi.discount as amount,
+	case when v.objid is null then 0 else 1 end as voided
+from collectionvoucher cv 
+	inner join remittance rem on cv.objid = rem.collectionvoucherid
+	inner join cashreceipt cr on rem.objid = cr.remittanceid
+	inner join rptpayment rp on cr.objid = rp.receiptid 
+	inner join rptpayment_item rpi on rp.objid = rpi.parentid 
+	inner join rptledger rl on rp.refid = rl.objid 
+	inner join barangay b on rl.barangayid = b.objid 
+	inner join propertyclassification pc on rl.classification_objid = pc.objid 
+	left join cashreceipt_void v on cr.objid = v.receiptid 
+go
+
+create view vw_newly_assessed_property
+as 
+select
+	f.objid,
+	f.owner_name,
+	f.tdno,
+	b.name as barangay,
+	case 
+		when f.rputype = 'land' then 'LAND' 
+		when f.rputype = 'bldg' then 'BUILDING' 
+		when f.rputype = 'mach' then 'MACHINERY' 
+		when f.rputype = 'planttree' then 'PLANT/TREE' 
+		else 'MISCELLANEOUS'
+	end as rputype,
+	f.totalav,
+	f.effectivityyear
+from faas_list f 
+	inner join barangay b on f.barangayid = b.objid 
+where f.state in ('CURRENT', 'CANCELLED') 
+and f.txntype_objid = 'ND' 
+go
+
---
+++ appserver/etracs/dev_modules/rptis-2.5.03.02.01/rameses-gov-etracs-rptis-landtax-report-services.jar/sql/landtaxreport_real_property_payment.sql
@@ -0,0 +1,18 @@
+[getPayments]
+select 
+  owner_name,
+  tdno,
+  classification,
+  rputype,
+  barangay,
+  sum(amount) as amount 
+from vw_real_property_payment
+where ${filter}
+and voided = 0
+group by 
+  owner_name,
+  tdno,
+  classification,
+  rputype,
+  barangay
+order by owner_name, tdno
