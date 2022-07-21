0fb1b60c7654fc2c3f1dcb3866b3f4a70f912bf2
worgflores <worgflores@gmail.com>
2022-07-21 13:45:36 +0800

    bin: updated

--- bin/start.sh
+++ bin/start.sh
@@ -6,6 +6,8 @@ BASE_DIR=`pwd`
 
 cd $BASE_DIR/email/mail-primary && docker-compose up -d
 
+cd $BASE_DIR/email/mail-bpls && docker-compose up -d
+
 
 cd $BASE_DIR/system/notification && docker-compose up -d
 
@@ -18,7 +20,7 @@ cd $BASE_DIR/system/etracs-web && docker-compose up -d
 
 cd $BASE_DIR/appserver/etracs && docker-compose -f docker-compose.yml -f docker-compose-ext.yml up -d
 
-cd $BASE_DIR/appserver/epayment && docker-compose up -d
+cd $BASE_DIR/appserver/epayment && docker-compose -f docker-compose.yml -f docker-compose-ext.yml up -d
 
 
 cd $RUN_DIR
